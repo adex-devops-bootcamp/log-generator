70d033f4d1f74d20dd9edea9b3816b3048f7baa9
Yaman Man Shakya <103587873+yshakya@users.noreply.github.com>
2026-02-05 19:41:18 +0545

    add cloudwatch

--- app.py
+++ app.py
@@ -3,12 +3,20 @@ import random
 import time
 import logging
 import sys
+import os
 from datetime import datetime
 from logging.handlers import SocketHandler
+import watchtower
+import boto3
 
-LOGSTASH_HOST = "elk.tekbay.click"
-LOGSTASH_PORT = 5044  # match your logstash tcp input
+# ---- ENV VARS ----
+LOGSTASH_HOST = os.getenv("LOGSTASH_HOST")
+LOGSTASH_PORT = int(os.getenv("LOGSTASH_PORT", 0))
+AWS_REGION = os.getenv("AWS_REGION")
+LOG_GROUP = os.getenv("CW_LOG_GROUP")
+LOG_STREAM = os.getenv("CW_LOG_STREAM")
 
+# ---- Logger Setup ----
 logger = logging.getLogger("demo-app")
 logger.setLevel(logging.INFO)
 
@@ -18,7 +26,7 @@ class JsonFormatter(logging.Formatter):
             "timestamp": datetime.utcnow().isoformat(),
             "level": record.levelname,
             "service": "demo-app",
-            "message": record.getMessage(),
+            "message": record.getMessage()
         })
 
 formatter = JsonFormatter()
@@ -28,9 +36,21 @@ stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setFormatter(formatter)
 logger.addHandler(stdout_handler)
 
-# 2️⃣ Logstash TCP handler
-tcp_handler = SocketHandler(LOGSTASH_HOST, LOGSTASH_PORT)
-logger.addHandler(tcp_handler)
+# 2️⃣ Logstash TCP (only if provided)
+if LOGSTASH_HOST and LOGSTASH_PORT:
+    tcp_handler = SocketHandler(LOGSTASH_HOST, LOGSTASH_PORT)
+    logger.addHandler(tcp_handler)
+
+# 3️⃣ CloudWatch (only if provided)
+if AWS_REGION and LOG_GROUP:
+    boto3_client = boto3.client("logs", region_name=AWS_REGION)
+    cw_handler = watchtower.CloudWatchLogHandler(
+        boto3_client=boto3_client,
+        log_group=LOG_GROUP,
+        stream_name=LOG_STREAM or "app-stream"
+    )
+    cw_handler.setFormatter(formatter)
+    logger.addHandler(cw_handler)
 
 # ---- App Loop ----
 while True:
@@ -42,4 +62,4 @@ while True:
     else:
         logger.info(f"Action success: {action}")
 
-    time.sleep(5)
+    time.sleep(1)
